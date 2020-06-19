98c58cf971bbaac55572e22fd4f83adf0211e572
aulia <aulia175@gmail.com>
2020-06-19 14:38:41 +0700

    insyaallah done

--- harga.py
+++ harga.py
@@ -4,6 +4,8 @@ from provinsiKota import *
 from tableDataText import *
 import json
 import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -22,6 +24,7 @@ def index():
     harga_daerah = 0
     kota_tujuan = ""
     pangan_pilihan = ""
+    hargaPanganID = ""
     if request.method == 'POST':
         pangan_pilihan = request.form['pangan_pilihan']
         provinsi_pilihan = request.form['provinsi_pilihan']
@@ -29,6 +32,7 @@ def index():
         for i in valid_kota:
             if int(i["city_id"]) == int(provinsi_pilihan):
                 kota_tujuan = i["province"]
+                hargaPanganID= i["hargaPanganID"]
                 
         #cek input masuk atau tidak
         print(pangan_pilihan + " " + provinsi_pilihan + berat_pangan)
@@ -48,12 +52,42 @@ def index():
         print(list_jasa)
 
         #scrap website hargapangan lalu update harga_jakarta dan harga_daerah
+        path = "D:\Kampus\eai\EAI-Komparasi-harga-beli-daerah-dan-harga-beli-jakarta-plus-ongkir"
+        driver = webdriver.Firefox(path)
+        driver.get("https://hargapangan.id/tabel-harga/pasar-tradisional/daerah")        
+        provinsiJakarta = Select(driver.find_element_by_id("filter_province_ids"))
+        provinsiJakarta.select_by_value("13") # 13 = dki jakarta
+        submit = driver.find_element_by_id("btnReport")
+        submit.click()
+        htmlDumpJakarta = driver.page_source
+        print("HTML DUMP JAKARTA GET")
+        provinsiTarget = Select(driver.find_element_by_id("filter_province_ids"))
+        provinsiTarget.select_by_value(hargaPanganID) # 13 = dki jakarta
+        submit = driver.find_element_by_id("btnReport")
+        submit.click()
+        htmlDumpTujuan = driver.page_source
+        print("HTML DUMP TARGET GET")
+        driver.quit()
 
-        html=urlopen("https://hargapangan.id/tabel-harga/pasar-tradisional/daerah")
-        bs=BeautifulSoup(html, features="html.parser")
+
+        # html=urlopen("https://hargapangan.id/tabel-harga/pasar-tradisional/daerah")
+        list_pangan_numbered = {"beras": 'I',"daging ayam":'II',"daging sapi":'III',"telur":'IV',"bawang merah":'V', "bawang putih":'VI', "cabai merah":'VII', "cabai rawit":'VIII', "minyak goreng":'IX', "gula pasir":'X'}
+
+        bs=BeautifulSoup(htmlDumpJakarta, features="html.parser")
         daftar=bs.find("table",{"id": "report"})
         panganDataTable = tableDataText(daftar)
-        list_pangan_numbered = {"beras": 'I',"daging ayam":'II',"daging sapi":'III',"telur":'IV',"bawang merah":'V', "bawang putih":'VI', "cabai merah":'VII', "cabai rawit":'VIII', "minyak goreng":'IX', "gula pasir":'X'}
+        nomorPangan =list_pangan_numbered.get(pangan_pilihan)
+        target = []
+
+        for cari in panganDataTable :
+            if cari[0] == nomorPangan :
+                target = cari
+        hargaTarget = int(target[2].replace(".",""))
+        harga_jakarta = hargaTarget
+
+        bs=BeautifulSoup(htmlDumpTujuan, features="html.parser")
+        daftar=bs.find("table",{"id": "report"})
+        panganDataTable = tableDataText(daftar)
         nomorPangan =list_pangan_numbered.get(pangan_pilihan)
         target = []
 
@@ -62,7 +96,7 @@ def index():
                 target = cari
         hargaTarget = int(target[2].replace(".",""))
         harga_daerah = hargaTarget
-        harga_jakarta = hargaTarget
+
 
         print(hargaTarget)
 
--- provinsiKota.py
+++ provinsiKota.py
@@ -5,7 +5,8 @@ id_kota_atau_kabupaten = [
                 "province": "Nanggroe Aceh Darussalam (NAD)",
                 "type": "Kota",
                 "city_name": "Banda Aceh",
-                "postal_code": "23238"
+                "postal_code": "23238",
+                "hargaPanganID" : "1"
             },
             
                 {"city_id": "278",
@@ -13,7 +14,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sumatera Utara",
                 "type": "Kota",
                 "city_name": "Medan",
-                "postal_code": "20228"
+                "postal_code": "20228",
+                "hargaPanganID" : "2"
             },
                         
                 {"city_id": "318",
@@ -21,7 +23,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sumatera Barat",
                 "type": "Kota",
                 "city_name": "Padang",
-                "postal_code": "25112"
+                "postal_code": "25112",
+                "hargaPanganID" : "3"
             },
                        
                 {"city_id": "350",
@@ -29,7 +32,8 @@ id_kota_atau_kabupaten = [
                 "province": "Riau",
                 "type": "Kota",
                 "city_name": "Pekanbaru",
-                "postal_code": "28112"
+                "postal_code": "28112",
+                "hargaPanganID" : "4"
             },
                         
                 {"city_id": "48",
@@ -37,7 +41,8 @@ id_kota_atau_kabupaten = [
                 "province": "Kepulauan Riau",
                 "type": "Kota",
                 "city_name": "Batam",
-                "postal_code": "29413"
+                "postal_code": "29413",
+                "hargaPanganID" : "5"
             },
                         
                 {"city_id": "156",
@@ -45,7 +50,8 @@ id_kota_atau_kabupaten = [
                 "province": "Jambi",
                 "type": "Kota",
                 "city_name": "Jambi",
-                "postal_code": "36111"
+                "postal_code": "36111",
+                "hargaPanganID" : "6"
             },
                         
                 {"city_id": "62",
@@ -53,7 +59,8 @@ id_kota_atau_kabupaten = [
                 "province": "Bengkulu",
                 "type": "Kota",
                 "city_name": "Bengkulu",
-                "postal_code": "38229"
+                "postal_code": "38229",
+                "hargaPanganID" : "7"
             },
                         
                 {"city_id": "327",
@@ -61,7 +68,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sumatera Selatan",
                 "type": "Kota",
                 "city_name": "Palembang",
-                "postal_code": "30111"
+                "postal_code": "30111",
+                "hargaPanganID" : "8"
             },
                         
                 {"city_id": "334",
@@ -69,7 +77,8 @@ id_kota_atau_kabupaten = [
                 "province": "Bangka Belitung",
                 "type": "Kota",
                 "city_name": "Pangkal Pinang",
-                "postal_code": "33115"
+                "postal_code": "33115",
+                "hargaPanganID" : "9"
             },
                         
                 {"city_id": "21",
@@ -77,7 +86,8 @@ id_kota_atau_kabupaten = [
                 "province": "Lampung",
                 "type": "Kota",
                 "city_name": "Bandar Lampung",
-                "postal_code": "35139"
+                "postal_code": "35139",
+                "hargaPanganID" : "10"
             },
                         
                 {"city_id": "403",
@@ -85,7 +95,8 @@ id_kota_atau_kabupaten = [
                 "province": "Banten",
                 "type": "Kota",
                 "city_name": "Serang",
-                "postal_code": "42111"
+                "postal_code": "42111",
+                "hargaPanganID" : "11"
             },
                         
                 {"city_id": "115",
@@ -93,7 +104,8 @@ id_kota_atau_kabupaten = [
                 "province": "Jawa Barat",
                 "type": "Kota",
                 "city_name": "Depok",
-                "postal_code": "16416"
+                "postal_code": "16416",
+                "hargaPanganID" : "12"
             },
                         
                 {"city_id": "152",
@@ -101,7 +113,8 @@ id_kota_atau_kabupaten = [
                 "province": "DKI Jakarta",
                 "type": "Kota",
                 "city_name": "Jakarta Pusat",
-                "postal_code": "10540"
+                "postal_code": "10540",
+                "hargaPanganID" : "13"
             },
                         
                 {"city_id": "399",
@@ -109,7 +122,8 @@ id_kota_atau_kabupaten = [
                 "province": "Jawa Tengah",
                 "type": "Kota",
                 "city_name": "Semarang",
-                "postal_code": "50135"
+                "postal_code": "50135",
+                "hargaPanganID" : "14"
             },
                         
                 {"city_id": "501",
@@ -117,7 +131,8 @@ id_kota_atau_kabupaten = [
                 "province": "DI Yogyakarta",
                 "type": "Kota",
                 "city_name": "Yogyakarta",
-                "postal_code": "55111"
+                "postal_code": "55111",
+                "hargaPanganID" : "15"
             },
                         
                 {"city_id": "444",
@@ -125,7 +140,8 @@ id_kota_atau_kabupaten = [
                 "province": "Jawa Timur",
                 "type": "Kota",
                 "city_name": "Surabaya",
-                "postal_code": "60119"
+                "postal_code": "60119",
+                "hargaPanganID" : "16"
             },
                        
                 {"city_id": "114",
@@ -133,7 +149,8 @@ id_kota_atau_kabupaten = [
                 "province": "Bali",
                 "type": "Kota",
                 "city_name": "Denpasar",
-                "postal_code": "80227"
+                "postal_code": "80227",
+                "hargaPanganID" : "17"
             },
                         
                 {"city_id": "276",
@@ -141,7 +158,8 @@ id_kota_atau_kabupaten = [
                 "province": "Nusa Tenggara Barat (NTB)",
                 "type": "Kota",
                 "city_name": "Mataram",
-                "postal_code": "83131"
+                "postal_code": "83131",
+                "hargaPanganID" : "18"
             },
                         
                 {"city_id": "213",
@@ -149,7 +167,8 @@ id_kota_atau_kabupaten = [
                 "province": "Nusa Tenggara Timur (NTT)",
                 "type": "Kota",
                 "city_name": "Kupang",
-                "postal_code": "85119"
+                "postal_code": "85119",
+                "hargaPanganID" : "19"
             },
                         
                 {"city_id": "365",
@@ -157,7 +176,8 @@ id_kota_atau_kabupaten = [
                 "province": "Kalimantan Barat",
                 "type": "Kota",
                 "city_name": "Pontianak",
-                "postal_code": "78112"
+                "postal_code": "78112",
+                "hargaPanganID" : "20"
             },
                         
                 {"city_id": "36",
@@ -165,7 +185,8 @@ id_kota_atau_kabupaten = [
                 "province": "Kalimantan Selatan",
                 "type": "Kota",
                 "city_name": "Banjarmasin",
-                "postal_code": "70117"
+                "postal_code": "70117",
+                "hargaPanganID" : "21"
             },
                         
                 {"city_id": "326",
@@ -173,7 +194,8 @@ id_kota_atau_kabupaten = [
                 "province": "Kalimantan Tengah",
                 "type": "Kota",
                 "city_name": "Palangka Raya",
-                "postal_code": "73112"
+                "postal_code": "73112",
+                "hargaPanganID" : "22"
             },
                         
                 {"city_id": "19",
@@ -181,7 +203,8 @@ id_kota_atau_kabupaten = [
                 "province": "Kalimantan Timur",
                 "type": "Kota",
                 "city_name": "Balikpapan",
-                "postal_code": "76111"
+                "postal_code": "76111",
+                "hargaPanganID" : "23"
             },
                         
                 {"city_id": "467",
@@ -189,7 +212,8 @@ id_kota_atau_kabupaten = [
                 "province": "Kalimantan Utara",
                 "type": "Kota",
                 "city_name": "Tarakan",
-                "postal_code": "77114"
+                "postal_code": "77114",
+                "hargaPanganID" : "24"
             },
                         
                 {"city_id": "130",
@@ -197,7 +221,8 @@ id_kota_atau_kabupaten = [
                 "province": "Gorontalo",
                 "type": "Kota",
                 "city_name": "Gorontalo",
-                "postal_code": "96115"
+                "postal_code": "96115",
+                "hargaPanganID" : "25"
             },
                         
                 {"city_id": "254",
@@ -205,7 +230,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sulawesi Selatan",
                 "type": "Kota",
                 "city_name": "Makassar",
-                "postal_code": "90111"
+                "postal_code": "90111",
+                "hargaPanganID" : "26"
             },
                         
                 {"city_id": "182",
@@ -213,7 +239,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sulawesi Tenggara",
                 "type": "Kota",
                 "city_name": "Kendari",
-                "postal_code": "93126"
+                "postal_code": "93126",
+                "hargaPanganID" : "27"
             },
                         
                 {"city_id": "329",
@@ -221,7 +248,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sulawesi Tengah",
                 "type": "Kota",
                 "city_name": "Palu",
-                "postal_code": "94111"
+                "postal_code": "94111",
+                "hargaPanganID" : "28"
             },
                         
                 {"city_id": "267",
@@ -229,7 +257,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sulawesi Utara",
                 "type": "Kota",
                 "city_name": "Manado",
-                "postal_code": "95247"
+                "postal_code": "95247",
+                "hargaPanganID" : "29"
             },
                         
                 {"city_id": "253",
@@ -237,7 +266,8 @@ id_kota_atau_kabupaten = [
                 "province": "Sulawesi Barat",
                 "type": "Kabupaten",
                 "city_name": "Majene",
-                "postal_code": "91411"
+                "postal_code": "91411",
+                "hargaPanganID" : "30"
             },
                         
                 {"city_id": "14",
@@ -245,7 +275,8 @@ id_kota_atau_kabupaten = [
                 "province": "Maluku",
                 "type": "Kota",
                 "city_name": "Ambon",
-                "postal_code": "97222"
+                "postal_code": "97222",
+                "hargaPanganID" : "31"
             },
                         
                 {"city_id": "477",
@@ -253,7 +284,8 @@ id_kota_atau_kabupaten = [
                 "province": "Maluku Utara",
                 "type": "Kota",
                 "city_name": "Ternate",
-                "postal_code": "97714"
+                "postal_code": "97714",
+                "hargaPanganID" : "32"
             },
                         
                 {"city_id": "158",
@@ -261,7 +293,8 @@ id_kota_atau_kabupaten = [
                 "province": "Papua",
                 "type": "Kota",
                 "city_name": "Jayapura",
-                "postal_code": "99114"
+                "postal_code": "99114",
+                "hargaPanganID" : "33"
             },
                         
                 {"city_id": "272",
@@ -269,6 +302,7 @@ id_kota_atau_kabupaten = [
                 "province": "Papua Barat",
                 "type": "Kabupaten",
                 "city_name": "Manokwari",
-                "postal_code": "98311"
+                "postal_code": "98311",
+                "hargaPanganID" : "34"
             },
 ]
